79ba23bb4d9989114e75f259fc94fc37d0975c00
sagar <sagarqaautomation@gmail.com>
2022-04-21 17:35:16 +0530

    commit

--- Sample.js
+++ Sample.js
@@ -9,16 +9,16 @@ let config ={
 
 //Page Locaters
 const login={
-	email:'input[id="userna"]',
+	email:'input[id="username"]',
 	password:'input[id="password"]'
 		}
 
 puppeteer.launch(config.launchOptions).then(async browser=> {
  const page=await browser.newPage();
- await page.goto('https://app.hubspot.com/login');
- await page.waitForSelector(login.email);
- await page.type(login.email,"puppeteer");
- await page.type(login.password,"puppeteer");
+ await page.goto('https://code-console.cerner.com/');
+//  await page.waitForSelector(login.email);
+//  await page.type(login.email,"puppeteer");
+//  await page.type(login.password,"puppeteer");
  await page.screenshot({path:'loginpage.png',fullPage:true})
 // if you want to generate pdf we need to run in headlessmode
  //await page.pdf({path:'loginpage.pdf'})
